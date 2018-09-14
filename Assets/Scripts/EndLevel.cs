3c09b983bd9efedec7bdb0f8a78a6ab65ffdf66c
Yaggdryzzill <felfig@globo.com>
2018-09-14 18:34:44 -0300

    Adicionado Fade in - Fade out para mudar níveis
    prefab de Fade criado. Ponto de final de níveis (EndLevel) criado.

---
+++ Assets/Scripts/EndLevel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevel : MonoBehaviour {
+    private GameObject fadeLevel;
+    private LevelChanger levelChanger;
+    public int levelIndex =1;
+	// Use this for initialization
+	void Start () {
+        fadeLevel = GameObject.Find("FadeLevel");
+        levelChanger = fadeLevel.GetComponent<LevelChanger>();
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            levelChanger.FadeToLevel(levelIndex);
+        }
+    }
+}
