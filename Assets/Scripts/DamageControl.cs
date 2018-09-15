be03d6eaef23b9b060ed9f6df8cb8fcfe9fd5dac
Yaggdryzzill <felfig@globo.com>
2018-09-15 10:40:02 -0300

    Bug Shark corrigido

--- Assets/Scripts/DamageControl.cs
+++ Assets/Scripts/DamageControl.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class DamageControl : MonoBehaviour {
     public GameObject Player;
+    public PlayerStateController playerScript;
     public int damage = 1;
 
     private void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
+        playerScript = Player.GetComponent<PlayerStateController>();
     }
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && playerScript.dead == false)
         {
             print("Damage1");
             Player.GetComponent<PlayerStateController>().Damaged(damage);
