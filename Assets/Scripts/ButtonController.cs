1fed319653963063716074aca6b9c2d5a9e3f8fb
TumaTi <jgsoaresdias@gmail.com>
2018-09-16 20:08:44 -0300

    Efeitos em todas as cenas

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour {
+
+    public AudioClip hoverSound;
+    private AudioSource _audio;
+
+    void Start()
+    {
+        _audio = GetComponent<AudioSource>();
+        if (_audio == null)
+        {
+            Debug.LogError("Need an AudioSource!!");
+        }
+    }
+
+    public void PlayOnHover()
+    {
+        _audio.PlayOneShot(hoverSound);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
